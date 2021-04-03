abd8e7d717d5c55f3367e8916b6e636c52d34e68
LagradOst <balt.758@gmail.com>
2021-04-03 13:29:29 +0200

    rename3

--- app/src/main/kotlin/com/lagradost/quicknovel/ui/roundedbg/TextRoundedBgAttributeReader.kt
+++ app/src/main/kotlin/com/lagradost/quicknovel/ui/roundedbg/TextRoundedBgAttributeReader.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-package com.lagradost.quicknovel.roundedbg
+package com.lagradost.quicknovel.ui.roundedbg
 
 import android.content.Context
 import android.graphics.drawable.Drawable
