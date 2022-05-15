d684798dd37ec28485d62738aa7280c4c86df995
AhmadNaruto <r.angka.kusuma@gmail.com>
2022-05-15 20:48:57 +0800

    add meionovel and more novel
    add sources -> meionovel and morenovel
    
    add sources -> meionovel and more novel
    add madara-base abstract

---
+++ app/src/main/java/com/lagradost/quicknovel/providers/MeioNovelProvider.kt
@@ -0,0 +1,8 @@
+package com.lagradost.quicknovel.providers
+
+import com.lagradost.quicknovel.MadaraInterface
+
+class MeioNovelProvider : MadaraInterface() {
+    override val name = "MeioNovel"
+    override val mainUrl = "https://meionovel.id"
+}
---
+++ app/src/main/java/com/lagradost/quicknovel/providers/MoreNovelProvider.kt
@@ -0,0 +1,8 @@
+package com.lagradost.quicknovel.providers
+
+import com.lagradost.quicknovel.MadaraInterface
+
+class MoreNovelProvider : MadaraInterface() {
+    override val name = "MoreNovel"
+    override val mainUrl = "https://morenovel.net"
+}
