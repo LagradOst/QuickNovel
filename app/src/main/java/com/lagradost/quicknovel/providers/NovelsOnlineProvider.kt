224586b8ea7c8e0dd7da6466f93043e3307174a8
KillerDogeEmpire <87155550+KillerDogeEmpire@users.noreply.github.com>
2022-08-24 17:59:56 -0600

    New Provider (#115)
    * New Provider
    
    * Added Links to My Forks
    
    * Update README.md
    
    * New provider
    
    * New provider
    
    * New provider and tags for readfrom.net
    
    * Bug Fixes
    
    * Bug Fixes
    
    * Bug Fixes and the correct logos
    
    * Bug Fixes and the correct logos
    
    * Bug Fixes and the correct logos
    
    * New Tags, since it doesn't show the Categories it is for the book.
    
    * Update README.md
    
    * Reset to main version for merge
    
    * Reset to main
    
    * Bug Fixes
    
    * Bug Fixes
    
    * Update README.md
    
    * Bug Fixes
    
    * Bug Fixes
    
    * New Provider Ranobes
    
    * Update README.md
    
    * Fix the Ranobes provider not loading any novel with 1000+ chapters
    
    * Test
    
    * Added New Provider, NovelFull.com
    
    * Update AllNovelProvider.kt
    
    * Update NovelFullProvider.kt
    
    * Added New Provider, NovelFull.com
    
    * Added 12 hr time format
    
    * teset
    
    * Idk
    
    * New Providers
    
    * merge with main, and update the background color of NovelFullVip's homepage so it's easier to distinguish from novelfull
    
    * merge with main
    
    * update the background color of NovelFullVip's homepage so it's easier to distinguish from novelfull, and added credit I forgot to do with the main PR
    
    * New Provider EngNovel
    
    * New Provider EngNovel
    
    * New Provider
    
    * bug fixes
    
    * bug fixes
    
    * bug fixes
    
    * bug fixes
    
    * bug fixes
    
    * Final comment ressolved
    
    * bug fixes

---
+++ app/src/main/java/com/lagradost/quicknovel/providers/NovelsOnlineProvider.kt
@@ -0,0 +1,150 @@
+package com.lagradost.quicknovel.providers
+import com.lagradost.quicknovel.*
+import com.lagradost.quicknovel.MainActivity.Companion.app
+import com.lagradost.quicknovel.network.CloudflareKiller
+import org.jsoup.Jsoup
+import kotlin.math.roundToInt
+
+class NovelsOnlineProvider : MainAPI() {
+    override val name = "NovelsOnline"
+    override val mainUrl = "https://novelsonline.net"
+    override val hasMainPage = true
+    override val iconId = R.drawable.icon_novelsonline
+    override val iconBackgroundId = R.color.wuxiaWorldOnlineColor
+
+    override val tags = listOf(
+        Pair("All", ""),
+        Pair("Action", "action"),
+        Pair("Adventure", "adventure"),
+        Pair("Celebrity", "celebrity"),
+        Pair("Comedy", "comedy"),
+        Pair("Drama", "drama"),
+        Pair("Ecchi", "ecchi"),
+        Pair("Fantasy", "fantasy"),
+        Pair("Gender Bender", "gender-bender"),
+        Pair("Harem", "harem"),
+        Pair("Historical", "historical"),
+        Pair("Horror", "horror"),
+        Pair("Josei", "josei"),
+        Pair("Martial Arts", "martial-arts"),
+        Pair("Mature", "mature"),
+        Pair("Mecha", "mecha"),
+        Pair("Mystery", "mystery"),
+        Pair("Psychological", "psychological"),
+        Pair("Romance", "romance"),
+        Pair("School Life", "school-life"),
+        Pair("Sci-fi", "sci-fi"),
+        Pair("Seinen", "seinen"),
+        Pair("Shotacon", "shotacon"),
+        Pair("Shoujo", "shoujo"),
+        Pair("Shoujo Ai", "shoujo-ai"),
+        Pair("Shounen", "shounen"),
+        Pair("Shounen Ai", "shounen-ai"),
+        Pair("Slice of Life", "slice-of-life"),
+        Pair("Sports", "sports"),
+        Pair("Supernatural", "supernatural"),
+        Pair("Tragedy", "tragedy"),
+        Pair("Wuxia", "wuxia"),
+        Pair("Xianxia", "xianxia"),
+        Pair("Xuanhuan", "xuanhuan"),
+        Pair("Yaoi", "yaoi"),
+        Pair("Yuri", "yuri"),
+    )
+    val interceptor = CloudflareKiller()
+
+    override suspend fun loadMainPage(
+        page: Int,
+        mainCategory: String?,
+        orderBy: String?,
+        tag: String?
+    ): HeadMainPageResponse {
+        val url =
+            if (tag.isNullOrBlank()) "$mainUrl/top-novel/$page" else "$mainUrl/category/$tag/$page"
+        
+        val document = app.get(url, interceptor = interceptor).document
+
+        val headers = document.select("div.top-novel-block")
+
+        val list = headers.map { h ->
+            val adata= h.selectFirst("div.top-novel-header > h2 > a")
+            val name = adata!!.text()
+            val cUrl = adata!!.attr("href")
+            val posterUrl = h.selectFirst("div.top-novel-content > div.top-novel-cover > a > img")!!.attr("src")
+
+                SearchResponse(
+                    name,
+                    cUrl,
+                    posterUrl,
+                    null,
+                    null,
+                    this.name, interceptor.getCookieHeaders(url).toMap()
+                )
+        }
+        return HeadMainPageResponse(url, list)
+    }
+
+    override suspend fun loadHtml(url: String): String? {
+        val response = app.get(url, interceptor = interceptor)
+        val document = Jsoup.parse(response.text.replace("Your browser does not support JavaScript!", ""))
+        return document.selectFirst("#contentall")!!.html()
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val document = app.post("$mainUrl/sResults.php",
+            interceptor= interceptor,
+            data = mapOf("q" to query)
+        ).document
+        val headers = document.select("li")
+        return headers.mapNotNull { h ->
+            val name = h.text()
+            val cUrl = h.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val posterUrl = h.selectFirst("img")?.attr("src")
+
+            SearchResponse(
+                name,
+                cUrl,
+                posterUrl,
+                null,
+                null,
+                this.name
+            )
+        }
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+
+        val document = app.get(url, interceptor = interceptor).document
+
+        val name = document.selectFirst("h1")!!.text()
+
+        val author = document.selectFirst("div.novel-details > div:nth-child(5) > div.novel-detail-body")?.select("li")?.map { it.text() }?.joinToString ()
+        val tags = document.selectFirst("div.novel-details > div:nth-child(2) > div.novel-detail-body")?.select("li")?.map { it.text() }
+
+        val posterUrl = document.select("div.novel-left > div.novel-cover > a > img").attr("src")
+        val synopsis = document.selectFirst("div.novel-right > div > div:nth-child(1) > div.novel-detail-body")?.text()
+
+        val data = document.select("ul.chapter-chs > li > a").map{c->
+            val cUrl =  c.attr("href")
+            val cName = c.text()
+            ChapterData(cName, cUrl, null, null)
+        }
+
+
+        val rating =
+            document.selectFirst("div.novel-right > div > div:nth-child(6) > div.novel-detail-body")?.text()?.toFloatOrNull()?.times(100)?.roundToInt()
+
+        return LoadResponse(
+            url,
+            name,
+            data,
+            author,
+            fixUrl(posterUrl),
+            rating,
+            null,
+            null,
+            synopsis,
+            tags,
+            null,
+        )
+    }
+}
