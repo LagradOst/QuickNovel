ea27400ee0bcdd3f463139c8d22acd240135aaae
LagradOst <11805592+LagradOst@users.noreply.github.com>
2023-08-10 17:19:17 +0200

    bump + added file for last commit

---
+++ app/src/main/java/com/lagradost/quicknovel/ui/ReadingType.kt
@@ -0,0 +1,16 @@
+package com.lagradost.quicknovel.ui
+
+import android.content.pm.ActivityInfo
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.lagradost.quicknovel.R
+
+enum class ReadingType(val prefValue: Int, @StringRes val stringRes: Int) {
+    DEFAULT(0, R.string.default_text),
+    INF_SCROLL(1, R.string.inf_scroll),
+    BTT_SCROLL(2, R.string.button_scroll);
+
+    companion object {
+        fun fromSpinner(position: Int?) = values().find { value -> value.prefValue == position } ?: DEFAULT
+    }
+}
